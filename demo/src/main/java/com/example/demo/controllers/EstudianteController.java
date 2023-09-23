ed2e7d43774778ba318750fc41953f3a1324a377
unknown <bruno.jaramillo@usach.cl>
2023-09-23 05:13:22 -0300

    Primera modificación de packages

---
+++ demo/src/main/java/com/example/demo/controllers/EstudianteController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.EstudianteEntity;
+import com.example.demo.services.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping
+public class EstudianteController {
+
+    private final EstudianteService estudianteService;
+
+    @Autowired
+    public EstudianteController(EstudianteService estudianteService) {
+        this.estudianteService = estudianteService;
+    }
+
+    @GetMapping("/formularioEstudiante")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("estudiante", new EstudianteEntity());
+        return "index";
+    }
+
+    @PostMapping("/guardarEstudiante")
+    public String guardarEstudiante(@RequestParam String rut,
+                                    @RequestParam String apellidos,
+                                    @RequestParam String nombres,
+                                    @RequestParam String fecha_nacimiento,
+                                    @RequestParam String tipo_colegio,
+                                    @RequestParam String nombre_colegio,
+                                    @RequestParam Integer anio_egreso) {
+        estudianteService.guardarEstudiante(rut, apellidos, nombres, fecha_nacimiento, tipo_colegio, nombre_colegio, anio_egreso);
+        return "index";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/services/EstudianteService.java
@@ -0,0 +1,30 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.EstudianteEntity;
+import com.example.demo.repositories.EstudianteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstudianteService {
+
+    private final EstudianteRepository estudianteRepository;
+
+    @Autowired
+    public EstudianteService(EstudianteRepository estudianteRepository) {
+        this.estudianteRepository = estudianteRepository;
+    }
+
+    public void guardarEstudiante(String rut, String apellidos, String nombres, String fecha_nacimiento, String tipo_colegio, String nombre_colegio, Integer anio_egreso) {
+        EstudianteEntity estudiante = new EstudianteEntity();
+        estudiante.setRut(rut);
+        estudiante.setApellidos(apellidos);
+        estudiante.setNombres(nombres);
+        estudiante.setFecha_nacimiento(fecha_nacimiento);
+        estudiante.setTipo_colegio(tipo_colegio);
+        estudiante.setNombre_colegio(nombre_colegio);
+        estudiante.setAnio_egreso(anio_egreso);
+        estudianteRepository.save(estudiante);
+    }
+
+}
